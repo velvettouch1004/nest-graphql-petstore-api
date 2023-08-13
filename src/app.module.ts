3d0987160c9f5c7aec1858da3daf883c2ac0c241
velvettouch1004 <velvettouch1004@gmail.com>
2023-08-13 18:41:31 +0000

    Setup TypeORM with sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
     PetsModule,
   ],
